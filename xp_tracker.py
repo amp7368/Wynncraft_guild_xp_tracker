56f48b02eb4779a219c2a6f26d03e18fe201c9b6
amp7368 <47309189+amp7368@users.noreply.github.com>
2019-10-24 19:08:23 -0400

    leader_board integration
    changed how leaderboard_data is returned to a dict
    allows get_xp_earned to return xp from leaderboard data as well

--- xp_tracker.py
+++ xp_tracker.py
@@ -31,7 +31,7 @@ def read_xp_chart():
     return chart
 
 
-leaderboard_cache = ['cache']
+leaderboard_cache = []
 guilds = dict()
 CYCLES_PER_UPDATE = 1
 PREFIX = "t!"
@@ -104,77 +104,130 @@ def read():
         guilds[element] = loaded[element]
 
 
-def fetch_leaderboard():
+async def fetch_leaderboard():
     try_number = 0
     while try_number < 5:
         try:
-            guild_data = loads(urllib.request.urlopen(
+            leader_board_raw_data = loads(urllib.request.urlopen(
                 "https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=guild&timeframe=alltime").readline().decode(
-                "utf-8"))
-            return guild_data
+                "utf-8"))['data']
+            leader_board_data = dict()
+            for guild_data in leader_board_raw_data:
+                leader_board_data[guild_data['name']] = guild_data
+            leaderboard_cache[0] = leader_board_data
         except:
             await asyncio.sleep(10)
         try_number += 1
-    raise Exception("FetchDataFailure")
 
 
 async def begin():
     await client.get_channel(BEGIN_CHANNEL).send("begun")
     read()
-    leaderboard_cache[0] = fetch_leaderboard()
     begun[0] = True
+    count = 0
+    await fetch_leaderboard()
     while True:
+        if count == 60:
+            count = 0
+            await fetch_leaderboard()
+
         start = time.time()
         for guild_name in guilds:
             guilds[guild_name]["time"] += 1
             if guilds[guild_name]["time"] == CYCLES_PER_UPDATE:
                 guilds[guild_name]["time"] = 0
-                guild_data = await fetch_data(guild_name)
 
-                current_date = time.time()
-                current_level = guild_data['level']
-                current_xp = guild_data['xp']
+                if guild_name in leaderboard_cache[0]:
+                    # put an exact amount of xp earned in
+                    data = leaderboard_cache[0][guild_name]
+                    date = time.time()
+                    xp = data['xp']
+                    level = data['level']
+                    current_data = {"type": "leader", "date": date, "level": level, "xp": xp}
+                    
+                else:
+                    guild_data = await fetch_data(guild_name)
 
-                past = guilds[guild_name]['xp'][-1]
-                past_date = past['date']
-                past_level = past['level']
-                past_xp = past['xp']
+                    current_date = time.time()
+                    current_level = guild_data['level']
+                    current_xp = guild_data['xp']
+                    current = {"type": "level", "date": current_date, "level": current_level, "xp": current_xp}
+                    guilds[guild_name]["xp"].append(current)
 
-                guilds[guild_name]["xp"].append(
-                    {"type": "level", "date": current_date, "level": current_level, "xp": current_xp})
+                    past = guilds[guild_name]['xp'][-1]
 
-                # get xp earned in the last cycle
-                await client.get_channel(guilds[guild_name]["channel"]).send(
-                    get_LL_xp_earned(past_date, past_level, past_xp, current_date, current_level, current_xp))
+                    # get xp earned in the last cycle
+                    await client.get_channel(guilds[guild_name]["channel"]).send(get_xp_earned(past, current))
         write()
         await asyncio.sleep(60 - (time.time() - start))
 
 
-def get_LL_xp_earned(past_date, past_level, past_xp, current_date, current_level, current_xp):
-    '''
-    get_level_level_xp_earned
-    :param past_date:
-    :param past_level:
-    :param past_xp:
-    :param current_date:
-    :param current_level:
-    :param current_xp:
-    :return:
-    '''
-    if past_level > current_level:
-        return "Past level is greater than current level? O.o"
-    xp_earned = 0
-    while past_level != current_level:
-        if past_level - 1 >= len(xp_chart):
-            return "msg appleptr16#5054 \"add level " + str(past_level) + " to the xp chart\""
-        xp_earned += xp_chart[past_level - 1] * (1 - past_xp / 100)
-        past_level += 1
-        past_xp = 0
-    xp_earned += ((current_xp - past_xp) / 100) * xp_chart[current_level]
-    days = str(
-        int(((datetime.fromtimestamp(int(current_date)) - datetime.fromtimestamp(int(past_date))).total_seconds() / (
-                60 * 60 * 24)) * 100) / 100)
-    return "xp earned in " + days + " days = " + str(int(xp_earned))
+def sum_xp(data):
+    xp_earned = data['xp']
+    for i in range(1, data['level']):
+        xp_earned += xp_chart[i]
+    return xp_earned
+
+
+def get_xp_earned(past, current):
+    if past['type'] == 'leader':
+        if current['type'] == 'leader':
+            # leader leader
+            xp_earned = current['xp'] - past['xp']
+            current_date = current['date']
+            past_date = past['date']
+
+            days = str(
+                int(((datetime.fromtimestamp(int(current_date)) - datetime.fromtimestamp(
+                    int(past_date))).total_seconds() / (60 * 60 * 24)) * 100) / 100)
+            return "xp earned in " + days + " days = " + str(int(xp_earned))
+        else:
+            # leader level
+            xp_earned = sum_xp(current) - past['xp']
+            current_date = current['date']
+            past_date = past['date']
+
+            days = str(
+                int(((datetime.fromtimestamp(int(current_date)) - datetime.fromtimestamp(
+                    int(past_date))).total_seconds() / (60 * 60 * 24)) * 100) / 100)
+            return "xp earned in " + days + " days = " + str(int(xp_earned))
+    else:
+        if current['type'] == 'leader':
+            # level leader
+            xp_earned = current['xp'] - sum_xp(past)
+            current_date = current['date']
+            past_date = past['date']
+
+            days = str(
+                int(((datetime.fromtimestamp(int(current_date)) - datetime.fromtimestamp(
+                    int(past_date))).total_seconds() / (60 * 60 * 24)) * 100) / 100)
+            return "xp earned in " + days + " days = " + str(int(xp_earned))
+        else:
+            # level level
+            if past['level'] > current['level']:
+                return "Past level is greater than current level? O.o"
+            xp_earned = 0
+
+            past_level = past['level']
+            past_xp = past['xp']
+            past_date = past['date']
+
+            current_level = current['level']
+            current_xp = current['xp']
+            current_date = current['date']
+
+            while past_level != current_level:
+                if past_level - 1 >= len(xp_chart):
+                    return "msg appleptr16#5054 \"add level " + str(past_level) + " to the xp chart\""
+                xp_earned += xp_chart[past_level - 1] * (1 - past_xp / 100)
+                past_level += 1
+                past_xp = 0
+            xp_earned += ((current_xp - past_xp) / 100) * xp_chart[current_level]
+            days = str(
+                int(((datetime.fromtimestamp(int(current_date)) - datetime.fromtimestamp(
+                    int(past_date))).total_seconds() / (
+                             60 * 60 * 24)) * 100) / 100)
+            return "xp earned in " + days + " days = " + str(int(xp_earned))
 
 
 async def end(message):
