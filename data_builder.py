4f93ac92864b9f6817dc20f44d3e6582d62f125a
amp7368 <47309189+amp7368@users.noreply.github.com>
2019-11-06 21:16:37 -0500

    builds data for Avicia

--- data_builder.py
+++ data_builder.py
@@ -3,8 +3,8 @@ from json import dumps
 
 xp = list()
 now = time.time()
-for i in (range(10000)):
-    xps = now - 60*60*24*20 + 60*60*24*i
+for i in (range(30)):
+    xps = now - 60*60*24*30 + 60*60*24*i
     # print(xps)
     entry = {"type":"leader","date":xps,"level": 52, "xp":i*50000000}
     xp.append(entry)
